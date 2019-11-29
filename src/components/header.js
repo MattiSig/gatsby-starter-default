cdf48e8ae68f659390026b143a30d1e0c04d2a54
Matthías Sigurbjörnsson <mattisigur@gmail.com>
2019-11-29 16:19:14 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          Breyttur titill
         </Link>
       </h1>
     </div>
